3a6fb68ce512727a56e6daa564e89785df345715
ghaith96 <ghaithjardane@gmail.com>
2026-03-28 16:41:11 +0300

    fix: enable edge-to-edge in ShareActivity

--- app/src/main/kotlin/com/linksink/ShareActivity.kt
+++ app/src/main/kotlin/com/linksink/ShareActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,6 +23,7 @@ class ShareActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         val app = application as LinkSinkApp
         viewModel = ShareViewModel(
